27f43df7457d6463ed54b99685978b26af7e750b
Machiel Visser <machielvisser@outlook.com>
2020-01-15 16:19:32 +0100

    Update Telegram.R

--- Telegram.R
+++ Telegram.R
@@ -16,12 +16,12 @@ send_telegram_document <- function(document, chat_id, bot_token){
   bot$sendDocument(document = document, chat_id = chat_id)
 }
 
-# send_telegram_message(text = "Your script is finished", 
+# send_telegram_message(text = "Your script is finished!", 
 #                       chat_id = chat_id, 
 #                       bot_token = bot_token)
 
 # send_telegram_photo(photo = "Image.png", 
-#                     caption = "Look at this awesome kitty!", 
+#                     caption = "Look at this awesome picture!", 
 #                     chat_id = chat_id, 
 #                     bot_token = bot_token)
 
